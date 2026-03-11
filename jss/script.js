38bcb8176b551ab2d6a04606b77d6602923e31ec
GiulianFabio7341 <you@example.com>
2026-03-11 16:06:49 -0300

    feat:prototipo

---
+++ jss/script.js
@@ -0,0 +1,265 @@
+function enviar(){
+
+let cidade = document.getElementById("cidadeSelect").value
+let ano = document.getElementById("anoSelect").value
+
+
+
+// IDEB e SAEB(N)
+fetch(`http://localhost:3000/ideb?id=${cidade}&ano=${ano}`)
+.then(res => res.json())
+.then(dados => {
+
+const dadosManuais = {
+
+"2900702": { // Alagoinhas
+
+"2023": {
+municipal: 3.1,
+publico: 3.9,
+aprmunicipal: 4.28,
+aprpublico: 4.33
+},
+
+"2021": {
+municipal: 4.5,
+publico: 4.2,
+aprmunicipal: 5.6,
+aprpublico: 4.6
+}
+
+},
+
+"2907509": { // Catu
+
+"2023": {
+municipal: 4.0,
+publico: 4.0,
+aprmunicipal: 4.47,
+aprpublico: 4.47
+},
+
+"2021": {
+municipal: 4.8,
+publico: 4.8,
+aprmunicipal: 4.91,
+aprpublico: 4.91
+}
+
+},
+
+"2902203": { // Aramari
+
+"2023": {
+municipal: 3.6,
+publico: 3.6,
+aprmunicipal: 3.97,
+aprpublico: 3.97
+},
+
+"2021": {
+municipal: "Sem dados até o momento",
+publico: "Sem dados até o momento",
+aprmunicipal: "Sem dados até o momento",
+aprpublico: "Sem dados até o momento"
+}
+
+},
+
+"2927408": { // Salvador
+
+"2023": {
+municipal: 4.2,
+publico: 3.9,
+aprmunicipal: 4.59,
+aprpublico: 4.28
+}
+
+,
+
+"2021": {
+municipal: 4.5,
+publico: 4.7,
+aprmunicipal: 4.80,
+aprpublico: 4.69
+} },
+
+"2925204": { // Pojuca
+
+"2023": {
+municipal: 4.1,
+publico: 4.1,
+aprmunicipal: 4.53,
+aprpublico: 4.53
+},
+
+"2021": {
+municipal: 4.8,
+publico: 4.8,
+aprmunicipal: 5.02,
+aprpublico: 5.02
+}
+
+},
+
+"2910503": { // Entre Rios
+
+"2023": {
+municipal: 2.8,
+publico: 2.8,
+aprmunicipal: 4.08,
+aprpublico: 4.08
+},
+
+"2021": {
+municipal: 3.7,
+publico: 3.7,
+aprmunicipal: 4.53,
+aprpublico: 4.50
+}
+
+
+},
+
+"2913705": { // Inhambupe
+
+"2023": {
+municipal: 3.5,
+publico: 3.5,
+aprmunicipal: 4.34,
+aprpublico: 4.34
+},
+
+"2021": {
+municipal: 3.6,
+publico: 3.6,
+aprmunicipal: 4.27,
+aprpublico: 4.27
+}
+
+
+},
+
+"2910800": { // Feira De Santana
+
+"2023": {
+municipal: 3.5,
+publico: 3.7,
+aprmunicipal: 4.52,
+aprpublico: 4.39
+},
+
+"2021": {
+municipal: "Sem dados até o momento",
+publico: 4.2,
+aprmunicipal: "Sem dados até o momento",
+aprpublico: 4.73
+}
+
+
+},
+
+"2902054": { // Araças
+
+"2023": {
+municipal: 3.8,
+publico: 3.8,
+aprmunicipal: 4.44,
+aprpublico: 4.44
+},
+
+"2021": {
+municipal: "Sem dados até o momento",
+publico: "Sem dados até o momento",
+aprmunicipal: "Sem dados até o momento",
+aprpublico: "Sem dados até o momento"
+}
+
+
+}
+
+
+
+}
+
+
+// se existir dado manual para a cidade e ano
+if (dadosManuais[cidade] && dadosManuais[cidade][ano]) {
+
+dados = dadosManuais[cidade][ano]
+
+}
+
+
+document.getElementById("idebMunicipal").innerText =
+"IDEB: " + (dados.municipal ?? "Não disponível")
+
+document.getElementById("idebPublico").innerText =
+"IDEB: " + (dados.publico ?? "Não disponível")
+
+document.getElementById("aprMunicipal").innerText =
+"(N): " + (dados.aprmunicipal ?? "Não disponível")
+
+document.getElementById("aprPublico").innerText =
+"(N): " + (dados.aprpublico ?? "Não disponível")
+
+})
+.catch(erro => {
+
+console.log("Erro IDEB:", erro)
+
+})
+
+
+// CENSO
+fetch(`http://localhost:3000/censo?id=${cidade}&ano=${ano}`)
+.then(res => res.json())
+.then(dados => {
+
+document.getElementById("censoMunicipal").innerText =
+"Municipal: " + dados.censomunicipal
+
+document.getElementById("censoPublico").innerText =
+"Público: " + dados.censopublica
+
+})
+.catch(erro => {
+
+console.log("Erro Censo:", erro)
+
+})
+
+
+// TAXA
+fetch(`http://localhost:3000/indicador?id=${cidade}&ano=${ano}`)
+.then(res => res.json())
+.then(dados => {
+
+document.getElementById("taxaMunicipalapr").innerText =
+"Rendimento: " + dados.taxa_aprmunicipal+"%"
+
+document.getElementById("taxaMunicipalrep").innerText =
+"Rendimento: " + dados.taxa_repmunicipal+"%"
+
+document.getElementById("taxaMunicipalaba").innerText =
+"Rendimento: " + dados.taxa_abamunicipal+"%"
+
+document.getElementById("taxaPublicoapr").innerText =
+"Rendimento: " + dados.taxa_aprpublica + "%"
+
+document.getElementById("taxaPublicorep").innerText =
+"Rendimento: " + dados.taxa_reppublica + "%"
+
+document.getElementById("taxaPublicoaba").innerText =
+"Rendimento: " + dados.taxa_abapublica + "%"
+
+})
+.catch(erro => {
+
+console.log("Erro taxa:", erro)
+
+})
+
+const tabela = document.getElementById('tabela')
+tabela.style.display = "block";
+}
---
+++ server.js
@@ -0,0 +1,218 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/ideb", async (req, res) => {
+
+    const id = req.query.id;
+    const ano = req.query.ano;
+
+    try {
+
+        const municipal = await axios.get("https://api.qedu.org.br/v1/ideb", {
+            params: {
+                id: id,
+                dependencia_id: 3,
+                ciclo_id: "AF",
+                ano: ano
+            },
+            headers: {
+                Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+            }
+        });
+        
+
+        const publico = await axios.get("https://api.qedu.org.br/v1/ideb", {
+            params: {
+                id: id,
+                dependencia_id: 5,
+                ciclo_id: "AF",
+                ano: ano
+            },
+            headers: {
+                Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+            }
+        });
+
+        res.json({
+            municipal: municipal.data.data[0].ideb,
+            publico: publico.data.data[0].ideb,
+            aprmunicipal:municipal.data.data[0].aprendizado,
+            aprpublico:publico.data.data[0].aprendizado
+          
+        });
+
+    } catch (error) {
+
+        console.log(error.response?.data || error.message);
+        res.status(500).json({ erro: "Erro ao buscar IDEB" });
+
+    }
+
+});
+
+app.get("/censo", async (req, res) => {
+
+    const id = req.query.id
+    const ano = req.query.ano
+
+    try {
+
+        const censomunicipal = await axios.get(
+            "https://api.qedu.org.br/v1/censo/territorio",
+            {
+                params: {
+                    ano: ano,
+                    dependencia_id: 3,
+                    ibge_id: id,
+                    oferta_id: 2
+                },
+                headers: {
+                    Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+                }
+            }
+        )
+
+        const censopublica = await axios.get(
+            "https://api.qedu.org.br/v1/censo/territorio",
+            {
+                params: {
+                    ano: ano,
+                    dependencia_id: 5,
+                    ibge_id: id,
+                    oferta_id: 2
+                },
+                headers: {
+                    Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+                }
+            }
+        )
+
+        res.json({
+           censomunicipal: censomunicipal.data.data[0].matriculas_anos_finais,
+            censopublica: censopublica.data.data[0].matriculas_anos_finais
+        })
+
+    } catch (erro) {
+
+        console.log(erro.response?.data || erro.message)
+
+        res.status(500).json({
+            erro: "Erro ao buscar censo"
+        })
+
+    }
+
+})
+
+app.get("/indicador", async (req, res) => {
+
+    const id = req.query.id
+    const ano = req.query.ano
+
+    try {
+
+        const series = [6,7,8,9]
+
+        let aprMunicipal = 0
+        let repMunicipal = 0
+        let abaMunicipal = 0
+
+        let aprPublica = 0
+        let repPublica = 0
+        let abaPublica = 0
+
+        let contMunicipal = 0
+        let contPublica = 0
+
+        for (let serie of series) {
+
+            const municipal = await axios.get(
+                "https://api.qedu.org.br/v1/indicador/tr/territorio",
+                {
+                    params:{
+                        ano: ano,
+                        dependencia_id: 3,
+                        ibge_id: id,
+                        localizacao_id: 0,
+                        serie_id: serie
+                    },
+                    headers:{
+                        Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+                    }
+                }
+            )
+
+            const publica = await axios.get(
+                "https://api.qedu.org.br/v1/indicador/tr/territorio",
+                {
+                    params:{
+                        ano: ano,
+                        dependencia_id: 5,
+                        ibge_id: id,
+                        localizacao_id: 0,
+                        serie_id: serie
+                    },
+                    headers:{
+                        Authorization: "Bearer 9ZjZqSoJShZKQAZs0yRu048gKOjVNBEGe8y6KBND"
+                    }
+                }
+            )
+
+            const m = municipal.data?.data?.[0]
+            const p = publica.data?.data?.[0]
+
+
+            if (m) {
+                aprMunicipal += Number(m.aprovados) || 0
+                repMunicipal += Number(m.reprovados) || 0
+                abaMunicipal += Number(m.abandonos) || 0
+                contMunicipal++
+            }
+
+            if (p) {
+                aprPublica += Number(p.aprovados) || 0
+                repPublica += Number(p.reprovados) || 0
+                abaPublica += Number(p.abandonos) || 0
+                contPublica++
+            }
+
+        }
+
+        res.json({
+
+            taxa_aprmunicipal: contMunicipal ? aprMunicipal / contMunicipal : null,
+            taxa_repmunicipal: contMunicipal ? repMunicipal / contMunicipal : null,
+            taxa_abamunicipal: contMunicipal ? abaMunicipal / contMunicipal : null,
+
+            taxa_aprpublica: contPublica ? aprPublica / contPublica : null,
+            taxa_reppublica: contPublica ? repPublica / contPublica : null,
+            taxa_abapublica: contPublica ? abaPublica / contPublica : null
+
+        })
+
+    } catch (erro) {
+
+        console.log(erro.response?.data || erro.message)
+
+        res.status(500).json({
+            erro: "Erro ao buscar taxa"
+        })
+
+    }
+
+})
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando");
+});
